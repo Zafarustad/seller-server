c5ee4cbc0559446d6925a5e51338d9579d284861
unknown <zafarustad123@gmail.com>
2021-05-05 14:47:02 +0530

    file-limit

--- server.js
+++ server.js
@@ -31,7 +31,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(
+  express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 })
+);
 
 const uri = process.env.ATLAS_URI;
 
@@ -72,11 +75,7 @@ app.get('/seller/inventory/:shopId', authToken, getShopInventory);
 app.put('/seller/verify/:shopId', authToken, markShopVerified);
 app.put('/seller/shop', authToken, updateShopDetails);
 app.put('/seller/shopImage', authToken, updateShopImage);
-app.put(
-  '/seller/inventory/availability',
-  authToken,
-  changeProductAvailability
-);
+app.put('/seller/inventory/availability', authToken, changeProductAvailability);
 app.delete(
   '/seller/shop/:shopId/product/:productId',
   authToken,
